39b6e5bedd17472a5e54f9916e8b16797f6db363
davidjb90 <buchanan.david12@gmail.com>
2016-04-22 00:57:00 +0000

    Updated pirate

--- pirate_bartender.py
+++ pirate_bartender.py
@@ -19,40 +19,46 @@ drink_names_adj = ["Salty", "Oily", "Cheeky", "Crusty"]
 drink_names_noun = ["Spitoon", "Gunwhale", "Brig", "Poopdeck"] 
 
 def what_drink(questions):  ### this function asks questions stored as values in the 'questions' dictionary above
-    preferences = {}        ### creates new dictionary 'preferences' with Boolean matched to key of 'questions' dictionary
-    for d, q in questions.items():
-        ques = input(q)
-        if ques == 'y' or ques == 'yes':
+    preferences = {}        ### creates new dictionary 'preferences' with Boolean mapped to key of 'questions' dictionary
+    for d, q in questions.items(): 
+        ques = input(q)                     ###based on user input and stored as ques
+        if ques == 'y' or ques == 'yes':    ###
             preferences[d] = True
         else:
             preferences[d] = False
     return preferences 
 
-def new_drink(preferences):   ###this function 
-    drink = []
-    for s, t in preferences.items():
+def new_drink(preferences):   ###this function creates a drink from ingredient lists located in "ingredients" dictionary
+    drink = []                ###it's checking Boolean values in dictionary created in what_drink function
+    for s, t in preferences.items():  
         if t == True:
-            drink.append(random.choice(ingredients[s]))
+            drink.append(random.choice(ingredients[s])) ###adds item from dictionary to new list called 'drinks' if boolean == True
         else:
             continue
     return drink
     
-def name_drink(drink_names_adj, drink_names_noun):
-    x = random.choice(drink_names_adj)
-    y = random.choice(drink_names_noun)
-    new_name = x + " " + y
+def name_drink(drink_names_adj, drink_names_noun):  ###this function takes lists above as arguments 
+    x = random.choice(drink_names_adj)              ###randomly selects item from list
+    y = random.choice(drink_names_noun)             ###repeats for noun list
+    new_name = x + " " + y                          ###local variable that stores the drink name and creates concatenated string with a space
     return new_name
     
 def main():
-    preferences = what_drink(questions)
-    drink = new_drink(preferences)
-    print("Based on what ye told me, here's what ye be drinkin': 'The {}'".format(name_drink(drink_names_adj, drink_names_noun)))
-    for thing in drink:
-        print('{}'.format(thing))
-    money = input("Arrrg, hope ye be satisfied. How's about another?")
-    if money == 'y' or money == 'yes':
-        main()
-    
+    drinking = True
+    while drinking:
+        preferences = what_drink(questions)             ###calls first function
+        drink = new_drink(preferences)
+        print("Based on what ye told me, here's what ye be drinkin': 'The {}'".format(name_drink(drink_names_adj, drink_names_noun)))
+        for thing in drink:
+            print('{}'.format(thing))
+        money = input("Arrrg, hope ye be satisfied. How's about another?")
+        if money == 'n' or money == 'no':
+            drinking = False ###escapes while loop if condition true
+        #main()
+        ####called main function using recursion
+        ###not considered good programming practice when handling large data sets or many imputs
+        ###stacking functions can overload memory
+        ###while loop will prevent this overload
     
 if __name__ == '__main__':
     main()
